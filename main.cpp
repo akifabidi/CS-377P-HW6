eaa2ee2d66782c1756f2948ccb9e945eb2f6673c
Akif Abidi <akif2002@cs.utexas.edu>
2022-11-19 16:33:51 -0600

    setting up 2.1

--- main.cpp
+++ main.cpp
@@ -9,12 +9,22 @@
 #include <sstream>
 #include <algorithm>
 #include <limits>
+#include <pthread.h>
 #include <math.h>
 
-
 using namespace std;
+#define MAX_THREADS 4
+struct graphDetails threadArg[MAX_THREADS];
+pthread_t handles[MAX_THREADS];
+pthread_mutex_t graph_lock;
 
-
+struct graphDetails {
+    int myId;
+    vector<int> rows;
+    vector<pair<int, int>> edgeDetails;
+    int numNodes;
+    int sourceNode;
+};
 
 void relaxEdge(int currentNode, int destNode, int edgeSize, vector<int> nextIteration, vector<int> lastIteration) {
     //cout << "before" << currentNode << "  " << destNode << "  " << edgeSize << "  " << nextIteration[destNode] << "  "<< lastIteration[currentNode] << endl;
@@ -70,9 +80,19 @@ int sequentialBf(vector<int> rows, vector<pair<int, int>> edgeDetails, int numNo
 
 
 // 2.1: course grain locking
-void* graphMutex(vector<int> rows, vector<pair<int, int>> edgeDetails, int numNodes, int sourceNode) {
-    pthread_mutex_t graph_lock;
+void* graphMutex(void* input) {
+    // TODO: Need to access graph details through thread args
+    // Currently working through a struct, see http://www.cse.cuhk.edu.hk/~ericlo/teaching/os/lab/9-PThread/Pass.html
 
+    // We define variables locally from the input, for sake of simplicity
+    int myId = (int) (((struct graphDetails*)input)->myId);;
+    vector<int> rows = (vector<int>) (((struct graphDetails *)input)->rows);
+    vector<pair<int, int>> edgeDetails = (vector<pair<int, int>>) (((struct graphDetails *)input)->edgeDetails);;
+    int numNodes = (int) (((struct graphDetails *)input)->numNodes);
+    int sourceNode = (int) (((struct graphDetails *)input)->sourceNode);
+    
+
+    
     vector<int> lastIteration(numNodes);
     vector<int> nextIteration(numNodes);
 
@@ -89,9 +109,10 @@ void* graphMutex(vector<int> rows, vector<pair<int, int>> edgeDetails, int numNo
 
     bool done = false;
     while(!done) {
-        int firstCol = 1;
+        int firstCol = myId;
         int lastCol;
-        for(int index = 1; index < rows.size(); index++) {
+        // THREAD DIVIDED BY NODES
+        for(int index = 1; index < rows.size(); index+= MAX_THREADS) {
             lastCol = rows[index];
 
             for(firstCol; firstCol <= lastCol; firstCol++) {
@@ -120,8 +141,7 @@ void* graphMutex(vector<int> rows, vector<pair<int, int>> edgeDetails, int numNo
 
 void* nodeMutex() { }
 
-
-int main(int argc, char *argv[]) {
+void* testSequentialBf() {
     vector<int> rows;
     vector<pair<int, int>> edgeDetails;
     rows.push_back(0);
@@ -141,5 +161,52 @@ int main(int argc, char *argv[]) {
     rows.push_back(8);
     
     sequentialBf(rows, edgeDetails, 6, 1);
+}
+
+
+int main(int argc, char *argv[]) {
+    // LOCK HANDELING
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_mutex_init(&graph_lock, NULL);
+
+    // BEGIN RUN TIME
+    uint64_t execTime; /*time in nanoseconds*/
+    struct timespec tick, tock;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
+
+    // DEFINE GRAPH
+    // TODO: need to translate from DIMAC file
+    struct graphDetails *masterGraph = (struct graphDetails *) malloc(sizeof(struct graphDetails));
+    // FILL IN WITH DETAILS
+    // graph -> rows = ;
+    // graph -> edgeDetails = ;
+    // graph -> numNodes;
+    // graph -> sourceNode;
+
+    //----------------------------------------------
+
+    // Part 2.1: Mutex on the graph
+    for(int i = 0; i < MAX_THREADS; i++) {
+        // TODO: I think we need to copy the graphDetails graph struct for every thread - becuase
+        // we need to have a unique myId for every thread. Because we pass the address of the struct
+        //we can't have one object. This is my attempt at it below
+        threadArg[i] = *masterGraph;
+        threadArg[i].myId = i;
+        pthread_create(&handles[i], &attr, graphMutex, &threadArg[i]);
+    }
+
+
+
+
+    //----------------------------------------------
+    // END RUN TIME
+    clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+    execTime = 1000000000 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
+    printf("\n ----PART 4---- \n elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)execTime);
+
+
+    pthread_exit(NULL);
+    return 0;
     
 }
